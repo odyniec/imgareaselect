8a136cd3ca48d969557af3b038a972c3e2ccbf4d
Michal Wojciechowski <odyniec@odyniec.net>
2012-11-25 22:30:13 +0100

    Add test module for keyboard support

---
+++ test/unit/keyboard.js
@@ -0,0 +1,33 @@
+module("Keyboard");
+
+(function () {
+
+test("Document keypress event binding", function () {
+    /* Initialization */
+    $('#t').append('<img id="test-img" src="data/elephant.jpg" />');
+
+    stop();
+
+    var keyPressed = false;
+
+    $(document).bind($.imgAreaSelect.keyPress, function () {
+        keyPressed = true;
+    });
+        
+    $('#test-img').imgAreaSelect({
+        onInit: function (img, selection) {
+            $(document).keydown();
+            $(document).keypress();
+
+            ok(keyPressed, 'Check if the original document keypress/keydown ' +
+                'event handler is called after plugin initialization');
+
+            $('#test-img').imgAreaSelect({ remove: true });
+            testCleanup();
+
+            start();
+        }
+    });
+});
+
+})();
