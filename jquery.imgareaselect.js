7b6f46346998e371a1e91f721230005139acbb86
Michal Wojciechowski <odyniec@odyniec.net>
2013-03-10 20:45:21 +0100

    Check event type to correctly handle touch events

--- jquery.imgareaselect.js
+++ jquery.imgareaselect.js
@@ -723,7 +723,9 @@ $.imgAreaSelect = function (img, options) {
      */
     function imgMouseDown(event) {
         /* Ignore the event if animation is in progress */
-        if (event.which != 1 || $outer.is(':animated')) return false;
+        if (event.type == 'mousedown' && event.which != 1 ||
+                $outer.is(':animated'))
+            return false;
 
         adjust();
         startX = x1 = evX(event);
